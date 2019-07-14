ad66150a3e757a6edc91ebd635f615005d48d0c8
Carl Argabright <carl.argabright@gmail.com>
2019-07-13 21:26:01 -0700

    final commit before submission of dykstra5

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ContosoUniversity.Models {
 
     public enum Grade {
 
-        A, B, C, D, F
+        A,
+        B,
+        C,
+        D,
+        F
 
     }
 
@@ -11,6 +18,8 @@ namespace ContosoUniversity.Models {
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
