ce8adbbf9a4c75976ceb9ddab6fed4b899797c8f
Carl Argabright <carl.argabright@gmail.com>
2019-07-27 23:53:27 -0700

    Final commit before submitting dykstra9

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,20 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models {
 
-    public class Student {
-
-        public int ID { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        public string FirstMidName { get; set; }
+    public class Student : Person {
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
